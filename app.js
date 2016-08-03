cbc494d5fef8ac26f2281c592327606eb5b51799
Timothy Do <timothy.do@gmail.com>
2016-08-03 10:00:13 -0700

    refactoring basic functionality

--- app.js
+++ app.js
@@ -1,103 +1,124 @@
+'use strict';
+
 window.onload = function() {
-  var page = 0;
-  var url = 'https://api.twitch.tv/kraken/';
-  var prev = null;
-  var selfURI = null;
-  var next = null;
+  var _env = {
+    page: 0,
+    url: 'https://api.twitch.tv/kraken/',
+    prev: null,
+    selfURI: null,
+    next: null,
+  };
 
   document.getElementById('prev_page').onclick = PrevPage;
   document.getElementById('next_page').onclick = NextPage;
 
   document.getElementById('query_box').onsubmit = function() {
-    var target = document.getElementById('query_text').value
-    var formValid = document.getElementById('form_valid')
-    var formInvalid = document.getElementById('form_invalid')
+    var target = document.getElementById('query_text').value;
+    var formValid = document.getElementById('form_valid');
+    var formInvalid = document.getElementById('form_invalid');
     if (target.length < 1) {
-      formValid.className = ''
-      formValid.innerHTML = ''
-      formInvalid.className = 'invalid'
-      formInvalid.innerHTML = 'Please enter a query!'
-      console.log('Invalid search')
+      formValid.className = '';
+      formValid.innerHTML = '';
+      formInvalid.className = 'invalid';
+      formInvalid.innerHTML = 'Please enter a query!';
+      console.log('Invalid search');
     } else {
-      // formInvalid.className = ''
-      formInvalid.className = ''
-      formInvalid.innerHTML = ''
-      formValid.className = 'valid'
-      // formValid.innerHTML = 'Please enter a query!'
-      formValid.innerHTML = 'Searching for "' + target + '".'
-      console.log('Valid search')
-      SearchAPI(url + 'search/streams?q=' + target);
+      formInvalid.className = '';
+      formInvalid.innerHTML = '';
+      formValid.className = 'valid';
+      formValid.innerHTML = 'Searching for "' + target + '".';
+      console.log('Valid search');
+      SearchAPI(_env.url + 'search/streams?q=' + target);
     }
     return false;
-  }
+  };
 
   function SearchAPI(query) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', query)
-    xhr.setRequestHeader('Client-ID', 'n3w0us084we2q25klwyasqv5dp1kmb1')
+    xhr.open('GET', query);
+    xhr.setRequestHeader('Client-ID', 'n3w0us084we2q25klwyasqv5dp1kmb1'); //Twitch API Key
     xhr.send(null);
 
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
           var data = JSON.parse(xhr.responseText);
-          document.getElementById('streams_list').innerHTML = '';
+          // document.getElementById('streams_list').innerHTML = '';
           FillBodyBox(data);
         } else {
           console.log('error : ', xhr.status);
         }
       }
-      console.log('Received all entries');
-    }
+      // console.log('Received all entries');
+    };
   }
 
   function FillBodyBox(data) {
-    selfURI = data._links.self;
+    _env.selfURI = data._links.self;
 
     var total = data._total;
-    var offset = parseURI('offset',selfURI);
-    var limit = parseURI('limit',selfURI)
-    var lastPage = Math.ceil(total/9)
+    var offset = parseURI('offset',_env.selfURI);
+    var limit = parseURI('limit',_env.selfURI);
+    var lastPage = Math.ceil(total/9);
+    var htmlString;
+    var streamList, oldList;
+    var $bodyBox = document.getElementById('body_box');
 
-    page = offset/limit + 1
-    prev = (page <= 1) ? null : data._links.prev;
-    next = (page >= lastPage) ? null : data._links.next;
-    
+    //Set the new page number value based on offset and total videos
+    _env.page = offset/limit + 1;
+
+    //Set new html links for previous and next page if exists
+    _env.prev = (_env.page <= 1) ? null : data._links.prev;
+    _env.next = (_env.page >= lastPage) ? null : data._links.next;
     document.getElementById('total_results').textContent = 'Total results: ' + total;
-    document.getElementById('curr_page').textContent = page + '/' + lastPage;
-    console.log(data);
-    data.streams.forEach(function(entry, index) {
-      // console.log(entry);
-      CreateTwitchEntry(entry);
-    })
+    document.getElementById('curr_page').textContent = _env.page + '/' + lastPage;
+    
+    htmlString = '';
+    data.streams.forEach(function(entry) {
+      htmlString += CreateTwitchEntry(entry);
+    });
+
+    //create a new list of streams
+    streamList = document.createElement('div');
+    streamList.className = 'streams_list';
+    streamList.id = 'page_' + _env.page;
+    streamList.innerHTML = htmlString;
+    oldList = $bodyBox.children[2];
+    // var oldNode = document.querySelector('div.streams_list')
+
+    //hide the old list of streams (remove if one already exists) 
+
+    $bodyBox.insertBefore(streamList, oldList);    
+    $bodyBox.removeChild(oldList);
+    console.log(streamList, oldList);
+    // document.getElementById('body_box').replaceChild(streamList,oldNode);
   }
 
   function CreateTwitchEntry(entry) {
-    var streamEntry = '<img src="' + entry.preview.medium + '" class="stream_image">' +
-                      '<div class="stream_info">' +
-                      '  <div class="stream_name">' + entry.channel.display_name + '</div>' +
-                      '  <div class="stream_game">' + entry.channel.game + ' - ' + entry.channel.views + ' viewers</div>' +
-                      '  <div class="stream_description">' + entry.channel.status + '</div>' +
-                      '</div>'
-    var htmlString = document.createElement('div')
-    htmlString.className = 'stream_entry'
-    htmlString.innerHTML = streamEntry;
-    document.getElementById('streams_list').appendChild(htmlString);
+    var twitchEntry ='<div class="stream_entry">' +
+                     '  <img src="' + entry.preview.medium + '" class="stream_image">' +
+                     '  <div class="stream_info">' +
+                     '    <div class="stream_name">' + entry.channel.display_name + '</div>' +
+                     '    <div class="stream_game">' + entry.channel.game + ' - ' + entry.channel.views + ' viewers</div>' +
+                     '    <div class="stream_description">' + entry.channel.status + '</div>' +
+                     '  </div>' +
+                     '</div>';
+    return twitchEntry;
   }
 
   function NextPage() {
-    console.log('Next!')
-    if (next) {
-      SearchAPI(next);
+    console.log('Next!');
+    if (_env.next) {
+      SearchAPI(_env.next);
     } else {
       console.log('No next page!');
     }
   }
 
   function PrevPage() {
-    console.log('Prev!')
-    if (prev) {
-      SearchAPI(prev);
+    console.log('Prev!');
+    if (_env.prev) {
+      SearchAPI(_env.prev);
     } else {
       console.log('No prev page!');
     }
@@ -109,6 +130,6 @@ window.onload = function() {
     }
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
     var results = regex.exec(url);
-    return !results ? null : (!results[2] ? '' : decodeURIComponent(results[2].replace(/\+/g, " ")))
+    return !results ? null : (!results[2] ? '' : decodeURIComponent(results[2].replace(/\+/g, " ")));
   }
-}
+};
