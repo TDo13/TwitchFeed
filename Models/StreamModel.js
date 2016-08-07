74fdf1f9d9f65ff7504b087d48927918db967f2b
Timothy Do <timothy.do@gmail.com>
2016-08-07 13:43:24 -0700

    Removing legacy models.

--- Models/StreamModel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function StreamModel(entry) {
-  this._img = entry.preview.medium;
-  this._name = entry.channel.display_name;
-  this._game = entry.channel.game;
-  this._viewers = entry.viewers;
-  this._description = entry.channel.status;
-}
